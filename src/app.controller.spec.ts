1eb302d31bd8503e167414c0f2260d855c00d0bf
Adam Wojnarowski <adam.piotr.wojnarowski@gmail.com>
2023-03-12 17:52:26 +0100

    init add test and lint automation

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,4 +1,4 @@
-import { CacheModule } from '@nestjs/common';
+import { CacheModule, CACHE_MANAGER } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -10,7 +10,16 @@ describe('AppController', () => {
     const app: TestingModule = await Test.createTestingModule({
       imports: [CacheModule],
       controllers: [AppController],
-      providers: [AppService],
+      providers: [
+        AppService,
+        {
+          provide: CACHE_MANAGER,
+          useValue: {
+            get: () => 'any value',
+            set: () => jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
     appController = app.get<AppController>(AppController);
