7d4fb391b372dbf9e210592ae27bda134630a6e4
Adam Wojnarowski <adam.piotr.wojnarowski@gmail.com>
2023-03-15 20:56:37 +0100

    test

--- src/animals/animals.controller.spec.ts
+++ src/animals/animals.controller.spec.ts
@@ -1,17 +1,19 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { ParamsWithId } from 'src/validations/id.validator';
 import { AnimalsController } from './animals.controller';
+import { AnimalsService } from './animals.service';
 
 describe('AnimalsController', () => {
   let controller: AnimalsController;
+  let service: AnimalsService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      imports: [ParamsWithId],
       controllers: [AnimalsController],
+      providers: [AnimalsService],
     }).compile();
 
     controller = module.get<AnimalsController>(AnimalsController);
+    service = module.get<AnimalsService>(AnimalsService);
   });
 
   it('should be defined', () => {
