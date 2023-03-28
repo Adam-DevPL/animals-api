34930546f39ea1ba2cb0759faf2a1537712a960c
Adam Wojnarowski <adam.piotr.wojnarowski@gmail.com>
2023-03-28 21:07:46 +0200

    refactor: small cleaning and minor fixes

--- src/animals/animals.service.ts
+++ src/animals/animals.service.ts
@@ -6,37 +6,39 @@ import {
 } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model, Types } from 'mongoose';
-import { AnimalType } from 'src/types/animals.type';
+import { RedisCacheService } from 'src/redis-cache/redis-cache.service';
 import { AnimalTypeParam } from 'src/validations/type.validator';
 import { AnimalDto, UpdateAnimalDto, AnimalNameDto } from './dto/animal.dto';
-import { Animal, AnimalDocument, AnimalWithId } from './schemas/animal.schema';
+import { Animal, AnimalDocument } from './schemas/animal.schema';
 
 @Injectable()
 export class AnimalsService {
   constructor(
     @InjectModel(Animal.name) private animalModel: Model<AnimalDocument>,
+    private readonly redisCacheManager: RedisCacheService,
   ) {}
 
-  async findAll() {
+  async findAll(): Promise<Animal[]> {
     try {
-      return (await this.animalModel.find({})) as AnimalWithId[];
+      return await this.animalModel.find();
     } catch (err) {
-      console.error(err);
+      // console.error(err);
       throw new InternalServerErrorException();
     }
   }
 
-  async findOne(id: string) {
+  async findOne(id: string): Promise<Animal> {
     try {
       const animal: AnimalDocument = await this.animalModel.findById({
         _id: new Types.ObjectId(id),
       });
+
       if (!animal) {
         throw new NotFoundException();
       }
-      return animal as AnimalWithId;
+      return animal;
     } catch (err) {
-      console.error(err);
+      // console.error(err);
       if (err instanceof NotFoundException) {
         throw new NotFoundException();
       }
@@ -44,20 +46,24 @@ export class AnimalsService {
     }
   }
 
-  async update(id: string, animalData: UpdateAnimalDto) {
+  async update(id: string, animalData: UpdateAnimalDto): Promise<Animal> {
     try {
       const animal: AnimalDocument = await this.animalModel.findByIdAndUpdate(
         { _id: new Types.ObjectId(id) },
         animalData,
-        { new: true },
+        {
+          new: true,
+        },
       );
 
       if (!animal) {
         throw new NotFoundException();
       }
 
-      return animal as AnimalWithId;
+      await this.redisCacheManager.clearCache();
+      return animal;
     } catch (err) {
+      console.error(err);
       if (err instanceof NotFoundException) {
         throw new NotFoundException();
       }
@@ -65,11 +71,11 @@ export class AnimalsService {
     }
   }
 
-  async create(animalData: AnimalDto) {
+  async create(animalData: AnimalDto): Promise<Animal> {
     try {
       const animal: AnimalDocument = await this.animalModel.findOne({
-        animalName: { $q: animalData.animalName },
-        animalType: { $q: animalData.type },
+        animalName: { $eq: animalData.animalName },
+        animalType: { $eq: animalData.type },
       });
 
       if (animal) {
@@ -81,9 +87,11 @@ export class AnimalsService {
         ...animalData,
       });
 
-      return newAnimal as AnimalWithId;
+      await this.redisCacheManager.clearCache();
+
+      return newAnimal;
     } catch (err) {
-      console.error(err);
+      // console.error(err);
       if (err instanceof BadRequestException) {
         throw new BadRequestException(err.message);
       }
@@ -91,10 +99,10 @@ export class AnimalsService {
     }
   }
 
-  async addAnimals(animalsList: AnimalDto[]) {
+  async addAnimals(animalsList: AnimalDto[]): Promise<Animal[]> {
     try {
-      const allAnimals: AnimalWithId[] = await this.findAll();
-      const alreadyExistAnimals = animalsList.filter((animal) =>
+      const allAnimals: Animal[] = await this.findAll();
+      const alreadyExistAnimals: AnimalDto[] = animalsList.filter((animal) =>
         allAnimals.some(
           ({ animalName, type }) =>
             animal.animalName === animalName && animal.type === type,
@@ -103,11 +111,14 @@ export class AnimalsService {
       if (alreadyExistAnimals.length !== 0) {
         throw new BadRequestException('Animals already exist in database');
       }
+
+      await this.redisCacheManager.clearCache();
+
       return await this.animalModel.insertMany(
         animalsList.map((animal) => ({ createdAt: new Date(), ...animal })),
       );
     } catch (err) {
-      console.error(err);
+      // console.error(err);
       if (err instanceof BadRequestException) {
         throw new BadRequestException(err.message);
       }
@@ -117,18 +128,22 @@ export class AnimalsService {
   async addAnimalsWithOneType(
     animalsNames: AnimalNameDto[],
     typeAnimal: AnimalTypeParam,
-  ) {
+  ): Promise<Animal[]> {
     try {
-      const allAnimals: AnimalWithId[] = await this.findAll();
-      const alreadyExistAnimals = animalsNames.filter((animal) =>
-        allAnimals.some(
-          ({ animalName, type }) =>
-            animal.animalName === animalName && typeAnimal.type === type,
-        ),
+      const allAnimals: Animal[] = await this.findAll();
+      const alreadyExistAnimals: AnimalNameDto[] = animalsNames.filter(
+        (animal) =>
+          allAnimals.some(
+            ({ animalName, type }) =>
+              animal.animalName === animalName && typeAnimal.type === type,
+          ),
       );
       if (alreadyExistAnimals.length !== 0) {
         throw new BadRequestException('Animals already exist in database');
       }
+
+      await this.redisCacheManager.clearCache();
+
       return await this.animalModel.insertMany(
         animalsNames.map((animal) => ({
           createdAt: new Date(),
@@ -137,7 +152,7 @@ export class AnimalsService {
         })),
       );
     } catch (err) {
-      console.error(err);
+      // console.error(err);
       if (err instanceof BadRequestException) {
         throw new BadRequestException(err.message);
       }
--- src/redis-cache/redis-cache.service.ts
+++ src/redis-cache/redis-cache.service.ts
@@ -3,11 +3,10 @@ import { Cache } from 'cache-manager';
 
 @Injectable()
 export class RedisCacheService {
-  // constructor(@Inject(CACHE_MANAGER) private readonly cache: Cache) {}
-  // async get(key: string) {
-  //   return await this.cache.get(key);
-  // }
-  // async set(key: string, value: unknown) {
-  //   await this.cache.set(key, value);
-  // }
+  constructor(@Inject(CACHE_MANAGER) private readonly cacheManager: Cache) {}
+
+  async clearCache() {
+    this.cacheManager.del('findAll');
+    this.cacheManager.del('findOne');
+  }
 }
